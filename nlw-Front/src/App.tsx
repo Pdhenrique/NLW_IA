6fa1ef09a17f90027cdec26290d0e076c411cf59
PedroHenriqueSS <129234362+Pdhenrique@users.noreply.github.com>
2023-09-14 18:06:15 -0300

    feat: add textAreas

--- nlw-Front/src/App.tsx
+++ nlw-Front/src/App.tsx
@@ -1,10 +1,59 @@
-import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 function App() {
-
   return (
-    <Button>CLick me</Button>
-  )
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com S2 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 m-2"></Github>
+            github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteudo da transcrição do video
+            selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6 ">
+          <form className="space-y-6">
+            <label htmlFor="video">Carregar vídeo</label>
+
+            <input type="file" id="video" accept="video/mp4"/>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
